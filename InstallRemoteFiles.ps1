b0acbf67fd62e013d002043deb874539d3840cd1
Tim Callaghan <spoida@yahoo.com>
2019-06-30 18:39:07 +1000

    Add file copy helper script

---
+++ InstallRemoteFiles.ps1
@@ -0,0 +1,9 @@
+Copy-Item -force -Path '.\Maps\Native Instruments\Komplete Kontrol MCU.remotemap' -Destination 'C:\ProgramData\Propellerhead Software\Remote\Maps\Native Instruments\'
+Copy-Item -force -Path '.\Maps\Native Instruments\Komplete Kontrol S61 MK2.remotemap' -Destination 'C:\ProgramData\Propellerhead Software\Remote\Maps\Native Instruments\'
+
+Copy-Item -force -Path '.\Codecs\Lua Codecs\Native Instruments\Komplete Kontrol MCU.lua' -Destination 'C:\ProgramData\Propellerhead Software\Remote\Codecs\Lua Codecs\Native Instruments\'
+Copy-Item -force -Path '.\Codecs\Lua Codecs\Native Instruments\Komplete Kontrol MCU.luacodec' -Destination 'C:\ProgramData\Propellerhead Software\Remote\Codecs\Lua Codecs\Native Instruments\'
+Copy-Item -force -Path '.\Codecs\Lua Codecs\Native Instruments\Komplete Kontrol MCU.png' -Destination 'C:\ProgramData\Propellerhead Software\Remote\Codecs\Lua Codecs\Native Instruments\'
+Copy-Item -force -Path '.\Codecs\Lua Codecs\Native Instruments\Komplete Kontrol S61 MK2.lua' -Destination 'C:\ProgramData\Propellerhead Software\Remote\Codecs\Lua Codecs\Native Instruments\'
+Copy-Item -force -Path '.\Codecs\Lua Codecs\Native Instruments\Komplete Kontrol S61 MK2.luacodec' -Destination 'C:\ProgramData\Propellerhead Software\Remote\Codecs\Lua Codecs\Native Instruments\'
+Copy-Item -force -Path '.\Codecs\Lua Codecs\Native Instruments\Komplete Kontrol S61 MK2.png' -Destination 'C:\ProgramData\Propellerhead Software\Remote\Codecs\Lua Codecs\Native Instruments\'
